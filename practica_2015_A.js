84376c8768c03a3f614b5f9ac4819fc60e07c755
X1010A <rodrigo.cayola.rojas@gmail.com>
2023-01-23 22:51:46 -0400

    se creo index.html principal y el archivo .js donde expresa los primeros algoritmos

---
+++ practica_2015_A.js
@@ -0,0 +1,37 @@
+/**
+ *                                           PRACTICA 2015-A
+ * 
+ *  1. Calcular la sumatoria de los N primeros números Naturales(Nota.- Los naturales no incluen al 0).
+ *  2. Calcular la sumatoria de los números enteros comprendidos en el rango [x,y].
+ *  3. Calcular la sumatoria de los números enteros comprendidos en el rango [x,y] que son múltiplos de tres.
+ *  4. calcular la sumatoria de los números enteros comprendidos en el rango [x,y] que son primos.
+ *  5. Calcular cuantos pares ordenados (a,b) de numeros enteros verifican 1990 < a^2 + b^2 < 1998
+ *  6. Calcular todas las ternas de numeros naturales (P1, P2, P3) tales que cada uno de ellos es menor o igual 
+ *  que 50, y es posible construir un triangulo con longithudes (P1,P2,P3)
+ *  7.Calcular todas las ternas de numeros naturales que son primos (P1,P2,P3) tales que cada uno de ellos es menor o igual
+ *  que 50, y es posible construir un triangulo con longitudes(P1,P2,P3)
+ *  8. Calcular cuantos numeros enteros positivos menores que 1020 tiene como unicos factores primos al 2,3 o 7?.
+ * Por ejemplo: 2,8,21,63,84,....(Nota: los numeros enteros positivos son 1,2,3,4,.....)
+ */
+
+
+// 1. Calcular la sumatoria de los N primeros números Naturales(Nota.- Los naturales no incluen al 0).
+function sumatoriaN(n){
+    let respuesta = 0
+    for (let i = 1; i <= n; i++){
+            
+        respuesta += i
+
+    }
+    return respuesta
+}
+
+// 2. Calcular la sumatoria de los números enteros comprendidos en el rango [x,y].
+
+function sumatoriaRangoXY(x, y){
+    for(x, x<=y || y <= x; ){
+
+
+    }
+
+}
